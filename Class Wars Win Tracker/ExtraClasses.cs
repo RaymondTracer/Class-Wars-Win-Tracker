abd14eff78c67ab50c5abe9f84db7095283edfab
Charlie Gadd <gaddc11@gmail.com>
2023-11-06 17:31:31 +1100

    Actually made it load the defaults faster.

--- Class Wars Win Tracker/ExtraClasses.cs
+++ Class Wars Win Tracker/ExtraClasses.cs
@@ -8,27 +8,59 @@ namespace Class_Wars_Win_Tracker
 {
     public class ClassWarsWinTracker
     {
-        public BluClass[] Blu { get; set; }
+        public BluClass[] Blu { get; } = new BluClass[9];
+
+        public ClassWarsWinTracker()
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                Blu[i] = new();
+            }
+        }
     }
 
     public class BluClass
     {
-        public RedClass[] Red { get; set; }
+        public RedClass[] Red { get; } = new RedClass[9];
+
+        public BluClass()
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                Red[i] = new();
+            }
+        }
     }
 
     public class RedClass
     {
-        public DustbowlStage[] Stage { get; set; }
+        public DustbowlStage[] Stage { get; } = new DustbowlStage[3];
+
+        public RedClass()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                Stage[i] = new();
+            }
+        }
     }
 
     public class DustbowlStage
     {
-        public StageControlPoint[] ControlPoint { get; set; }
+        public StageControlPoint[] ControlPoint { get; } = new StageControlPoint[2];
+
+        public DustbowlStage()
+        {
+            for (int i = 0; i < 2; i++)
+            {
+                ControlPoint[i] = new();
+            }
+        }
     }
 
     public class StageControlPoint
     {
-        public byte BluWins { get; set; }
-        public byte RedWins { get; set; }
+        public byte BluWins { get; set; } = 0;
+        public byte RedWins { get; set; } = 0;
     }
 }
